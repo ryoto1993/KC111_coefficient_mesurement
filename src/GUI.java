724a1c1c7de6f49c495f2aeaf7e94249a0fdc3ea
RyotoTomioka <tommy.ryoto93@gmail.com>
2016-05-27 07:19:23 +0900

    Initial commit

---
+++ src/GUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Created by RyotoTomioka on 2016/05/27.
+ */
+public class GUI extends JFrame {
+    GUI() {
+        this.setTitle("影響度測定支援ツール");
+        this.setSize(300, 200);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.setVisible(true);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by RyotoTomioka on 2016/05/27.
+ */
+public class Main {
+    public static void main(String[] args) {
+        new GUI();
+    }
+}
